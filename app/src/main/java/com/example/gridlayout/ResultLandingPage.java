87cf366ce6d1cc82cbff357af964f6372d618e84
letuanlelele <tmle@usc.edu>
2023-09-18 23:38:46 -0700

    Working version 3. Added course name and my name to the action bar of the landing page.

--- app/src/main/java/com/example/gridlayout/ResultLandingPage.java
+++ app/src/main/java/com/example/gridlayout/ResultLandingPage.java
@@ -2,6 +2,7 @@ package com.example.gridlayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,6 +13,8 @@ public class ResultLandingPage extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_landing_page);
+
+        initializeActionBar();
 
         TextView result = findViewById(R.id.result);
         Button play_again = findViewById(R.id.play_again);
@@ -29,4 +32,14 @@ public class ResultLandingPage extends AppCompatActivity{
             startActivity(intent);
         });
     }
+    private void initializeActionBar() {
+        // Inflate the custom action bar layout
+        View customActionBar = getLayoutInflater().inflate(R.layout.custom_actionbar, null);
+
+        // Set the custom action bar layout as the action bar view
+        getSupportActionBar().setCustomView(customActionBar);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // Hide the default title
+    }
 }
+
